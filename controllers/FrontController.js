b2d5c902f31ca18f6a2f18f4a260e38dfe3f26da
netwarp <resethread@gmail.com>
2020-11-23 13:17:47 +0100

    up front

--- controllers/FrontController.js
+++ controllers/FrontController.js
@@ -1,6 +1,12 @@
+const SmartContract = require('../models/SmartContract')
+
 exports.index = async (request, response) => {
-    let data = {
-        user: request.user
+
+    const smarts_contracts = await SmartContract.find({}).sort({created_at: -1})
+
+    const data = {
+        user: request.user,
+        smarts_contracts
     }
 
     response.render('index.html', data)
@@ -12,6 +18,17 @@ exports.search = async (request, response) => {
     }
 
     response.render('search.html', data)
+}
+
+exports.smartContract = async (request, response) => {
+    const id = await request.params.id
+    const smart_contract = await SmartContract.findById(id)
+
+    const data = {
+        smart_contract
+    }
+
+    response.render('smart-contract.html', data)
 }
 
 exports.login = async (request, response) => {
