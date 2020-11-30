840b51e9f3dcbf414da298723320820ce402420d
netwarp <resethread@gmail.com>
2020-11-30 13:36:26 +0100

    add repository id to SC model

--- models/SmartContract.js
+++ models/SmartContract.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const ObjectId = require('mongodb').ObjectID;
 
 const schema = new Schema({
     title: String,
     description: String,
     file: String,
+    version: String,
     content: String,
     user_id: Number,
     repository: String,
     username: String,
+
+    repository_id: ObjectId,
+
     created_at: {
         type: Date,
         default: Date.now
