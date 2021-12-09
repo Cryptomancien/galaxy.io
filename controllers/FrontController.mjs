82a814151b53b85719ef921b9b13ded38a79f25f
netwarp <resethread@gmail.com>
2021-12-09 15:18:05 +0100

    up

--- controllers/FrontController.mjs
+++ controllers/FrontController.mjs
@@ -24,9 +24,6 @@ const FrontController = {
         await response.render('index.html', data)
     },
 
-    async getStarted(request, response) {
-        response.render('get-started.html')
-    },
 
     async search(request, response) {
         const search = await request.query.search ?? ''
@@ -41,54 +38,29 @@ const FrontController = {
         }
 
         return await response.render('search.html', data)
+    },
+
+    async contract(request, response) {
+        const id = await request.params.id
+        const sql = `select contracts.*, users.username, users.avatar_url from contracts left join users on users.id = contracts.user_id WHERE contracts.id = ${id} LIMIT 1;`
+        let contract = await sequelize.query(sql, { type: QueryTypes.SELECT })
+        contract = contract[0]
+
+        const data = {
+            contract
+        }
+        response.json(data)
+        //response.render('contract.html', data)
     }
 }
 
 export default FrontController
 /*
-const User = require('../models/User')
-const Repository = require('../models/Repository')
-const Contract = require('../models/Contract')
 
-require('dotenv').config()
-const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres'
-})
 
-exports.index = async (request, response) => {
 
-    const sql = 'select contracts.*, users.username, users.avatar_url from contracts left join users on users.id = contracts.user_id order by id desc LIMIT 9;'
-    const contracts = await sequelize.query(sql, { type: QueryTypes.SELECT })
 
-    const data = {
-        user: request.user,
-        contracts
-    }
-
-    await response.render('index.html', data)
-}
-
-exports.getStarted = async (request, response) => {
-    response.render('get-started.html')
-}
-
-exports.search = async (request, response) => {
-
-    const search = await request.query.search ?? ''
-    const sql = 'select contracts.*, users.username, users.avatar_url from contracts left join users on users.id = contracts.user_id order by id desc;'
-
-    const contracts = await sequelize.query(sql, { type: QueryTypes.SELECT })
-
-    let data = {
-        user: request.user,
-        contracts
-    }
-
-    return await response.render('search.html', data)
-}
 
 exports.contract = async (request, response) => {
     const id = await request.params.id
@@ -132,18 +104,7 @@ exports.user = async (request, response) => {
     await response.render('user.html', data)
 }
 
-exports.login = async (request, response) => {
-    let data = {
-        isAuth: request.isAuthenticated()
-    }
 
-    response.render('login.html', data)
-}
-
-exports.logout = async (request, response) => {
-    request.logout()
-    response.redirect('/')
-}
 
 exports.play = async (request, response) => {
     response.render('play.html')
--- index.mjs
+++ index.mjs
@@ -85,8 +85,8 @@ passport.deserializeUser((user, done) =>  done(null, user) )
 
 
 app.get('/', FrontController.index)
-app.get('/get-started', FrontController.getStarted)
 app.get('/search', FrontController.search)
+app.get('/contract/:id', FrontController.contract)
 
 app.get('/login', LoginController.index)
 app.get('/login/github', passport.authenticate('github'))
@@ -107,16 +107,16 @@ app.get('/app', ensureAuthenticated, (request, response) => {
     response.render('app.html', data)
 })
 
-app.get('/api/contracts', ContractsController.all)
-app.post('/api/contracts', ContractsController.store)
-app.get('/api/contracts/:id', ContractsController.show)
-app.put('/api/contracts/:id', ContractsController.update)
-app.delete('/api/contracts/:id', ContractsController.destroy)
+app.get('/api/contracts',ensureAuthenticated, ContractsController.all)
+app.post('/api/contracts',ensureAuthenticated, ContractsController.store)
+app.get('/api/contracts/:id',ensureAuthenticated, ContractsController.show)
+app.put('/api/contracts/:id',ensureAuthenticated, ContractsController.update)
+app.delete('/api/contracts/:id',ensureAuthenticated, ContractsController.destroy)
 
 app.get('/logout', LoginController.logout)
 
 // TODO movie in api controller
-app.get('/api/logged-user', (request, response) => {
+app.get('/api/logged-user', ensureAuthenticated, (request, response) => {
     const user = request.user
     return response.json(user)
 })
@@ -125,100 +125,11 @@ app.listen(port, () => console.log(`http://localhost:${port}`))
 
 
 /*
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const port = 8080
-const  nunjucks = require('nunjucks')
-
-require('dotenv').config()
-
-const passport = require('passport')
-const session = require('express-session')
-const GithubStrategy = require('passport-github').Strategy
-
-
-const FrontController = require('./controllers/FrontController')
-const RepositoriesController = require('./controllers/Api/RepositoriesController')
-const ContractsController = require('./controllers/Api/ContractsController')
-const ensureAuthenticated = require('./middleware/ensureAuthenticated')
-
-nunjucks.configure('views', {
-	autoescape:  true,
-	express:  app,
-	watch: true,
-})
-
-
-const User = require('./models/User')
-
-passport.use(
-	new GithubStrategy(
-		{
-			clientID: process.env.clientID,
-			clientSecret: process.env.clientSecret,
-			callbackUrl: 'http://localhost:8080/login/github/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			return done(null, profile)
-		}
-	)
-)
-
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use(session({
-	secret: process.env.session,
-	proxy: true,
-	resave: true,
-	saveUninitialized: true
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-passport.serializeUser(async (user, done) => {
-
-	const u = await User.findByPk(user.id)
-	if (u === null) {
-
-		const data = {
-			id: user.id,
-			username: user.username,
-			avatar_url: user['_json']['avatar_url'],
-			provider: user.provider
-		}
-
-		await User.create(data)
-	}
-
-	await console.log(user)
-	await done(null, user)
-})
-
-passport.deserializeUser((user, done) =>  done(null, user) )
-
-app.get('/', FrontController.index)
-
-app.get('/get-started', FrontController.getStarted)
-
-app.get('/search', FrontController.search)
 
 app.get('/contract/:id', FrontController.contract)
 
 app.get('/user/:username', FrontController.user)
 
-app.get('/login', FrontController.login)
-
-app.get('/login/github', passport.authenticate('github'))
-
-app.get('/login/github/callback', passport.authenticate('github', { failureRedirect: '/nope' }),
-	(request, response) => {
-		response.redirect('/app')
-	}
-)
 
 app.get('/play', FrontController.play)
 
@@ -232,25 +143,4 @@ app.get('/app', ensureAuthenticated, (request, response) => {
 
 	response.render('app.html', data)
 })
-
-app.get('/logout', FrontController.logout)
-
-app.get('/api/repositories', RepositoriesController.index)
-app.post('/api/repositories', RepositoriesController.store)
-app.get('/api/repositories/:id', RepositoriesController.show)
-app.put('/api/repositories/:id', RepositoriesController.update)
-app.delete('/api/repositories/:id', RepositoriesController.destroy)
-
-
-
-
-app.use((request, response) => {
-	if (request.user) {
-		return response.redirect('/app')
-	}
-
-	return response.redirect('/')
-})
-
-
 */
